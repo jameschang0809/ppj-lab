5325e971811acfc6f91bb471b9bedc251d032cb2
James Chang <jameschang0809@gmail.com>
2015-09-16 17:45:43 +0800

    Add demo code for blog article.

---
+++ ResharperUnitTestRunnerAndBinFolder/ResharperUnitTestRunnerAndBinFolder/ResharperUnitTestRunnerTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace ResharperUnitTestRunnerAndBinFolder
+{
+    [TestFixture]
+    public class ResharperUnitTestRunnerTests
+    {
+        [Test]
+        public void LoadAssembliesFromBinFolder_Test()
+        {
+            // arrange
+            const int expected = 2;
+            var binPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // act 
+            var assemblies = Directory.GetFiles(binPath, "*.dll").Select(Assembly.LoadFile).ToArray();
+
+            // assert
+            Assert.AreEqual(expected, assemblies.Length);
+        }
+    }
+}
